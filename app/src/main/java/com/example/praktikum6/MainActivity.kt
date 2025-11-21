4afae8ccac272846f639e6c3296933abe8cfbdab
hikmatyar22 <hikmatyaralghifary421@gmail.com>
2025-11-21 12:39:09 +0700

    memodifikasi mainactivity dengan memanggil siswaapp yang ada di navigasi untuk menjalankan program

--- app/src/main/java/com/example/praktikum6/MainActivity.kt
+++ app/src/main/java/com/example/praktikum6/MainActivity.kt
@@ -6,29 +6,33 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.praktikum6.ui.theme.Praktikum6Theme
+import com.example.praktikum6.view.uicontroller.SiswaApp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            Praktikum6Theme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+            Praktikum6Theme{
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    SiswaApp(modifier = Modifier.fillMaxSize())
                 }
             }
         }
     }
 }
+
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
