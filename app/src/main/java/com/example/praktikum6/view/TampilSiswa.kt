8a1336269c75af9e7449db445e8564c2807eede5
hikmatyar22 <hikmatyaralghifary421@gmail.com>
2025-11-21 09:45:16 +0700

    Membuat scaffold untuk tampilan halaman tampil siswa dengan menggynakan topbar dan body(isiruang)

--- app/src/main/java/com/example/praktikum6/view/TampilSiswa.kt
+++ app/src/main/java/com/example/praktikum6/view/TampilSiswa.kt
@@ -35,6 +35,32 @@ fun TampilData(
         Pair(stringResource(id = R.string.jenis_kelamin),"Lainnya"),
         Pair(stringResource(id = R.string.alamat),"Yogyakarta"),
     )
-
+    Scaffold (modifier = Modifier,
+        topBar = {
+            TopAppBar(
+                title = { Text(text = stringResource(id= R.string.tampil), color = Color.White)},
+                colors = TopAppBarDefaults.mediumTopAppBarColors(colorResource(id= R.color.teal_700))
+            )
+        }){ isiRuang ->
+        Column(modifier = Modifier.padding(isiRuang),
+            verticalArrangement = Arrangement.SpaceBetween){
+            Column(modifier = Modifier.padding(all = dimensionResource(id = R.dimen.padding_medium)),
+                verticalArrangement = Arrangement.spacedBy(dimensionResource(id = R.dimen.padding_small))) {
+                items.forEach { item ->
+                    Column {
+                        Text(text = item.first.uppercase(), fontSize = 16.sp)
+                        Text(text = item.second, fontWeight = FontWeight.Bold, fontFamily = FontFamily.Cursive, fontSize = 22.sp)
+                    }
+                    HorizontalDivider(thickness = 1.dp, color = Color.Cyan)
+                }
+                Spacer(modifier = Modifier.height(10.dp))
+                Button(
+                    modifier = Modifier.fillMaxWidth(),
+                    onClick = onBackBtnClick){
+                    Text(text = stringResource(id= R.string.back))
+                }
+            }
+        }
+    }
 }
 
