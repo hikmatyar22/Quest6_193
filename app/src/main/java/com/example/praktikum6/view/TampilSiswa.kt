b72361fdf9580d6695d0d5f4f63574fcaf81fe99
hikmatyar22 <hikmatyaralghifary421@gmail.com>
2025-11-21 09:43:59 +0700

    Membuat beberapa variabel val  di TampilSiswa dengan memanggil beberapa string resource

--- app/src/main/java/com/example/praktikum6/view/TampilSiswa.kt
+++ app/src/main/java/com/example/praktikum6/view/TampilSiswa.kt
@@ -30,6 +30,11 @@ import com.example.praktikum6.R
 fun TampilData(
     onBackBtnClick:()->Unit
 ){
+    val items = listOf(
+        Pair(stringResource(id = R.string.nama_lengkap),"Contoh Nama"),
+        Pair(stringResource(id = R.string.jenis_kelamin),"Lainnya"),
+        Pair(stringResource(id = R.string.alamat),"Yogyakarta"),
+    )
 
 }
 
