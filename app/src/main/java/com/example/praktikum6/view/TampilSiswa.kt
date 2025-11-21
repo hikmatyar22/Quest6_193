074d6c2a6ed4952155b1e27ea973e34b451353b1
hikmatyar22 <hikmatyaralghifary421@gmail.com>
2025-11-21 10:57:26 +0700

    modifikasi dan tambahan pada TampilanSiswa

--- app/src/main/java/com/example/praktikum6/view/TampilSiswa.kt
+++ app/src/main/java/com/example/praktikum6/view/TampilSiswa.kt
@@ -24,28 +24,35 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.praktikum6.R
+import com.example.praktikum6.model.Siswa
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun TampilData(
+    //edit 1 = parameter satusUISiswa
+    statusUiSiswa: Siswa,
     onBackBtnClick:()->Unit
 ){
+    //edit 2
     val items = listOf(
-        Pair(stringResource(id = R.string.nama_lengkap),"Contoh Nama"),
-        Pair(stringResource(id = R.string.jenis_kelamin),"Lainnya"),
-        Pair(stringResource(id = R.string.alamat),"Yogyakarta"),
+        Pair(stringResource(id = R.string.nama_lengkap),statusUiSiswa.nama),
+        Pair(stringResource(id = R.string.jenis_kelamin),statusUiSiswa.gender),
+        Pair(stringResource(id = R.string.alamat),statusUiSiswa.alamat),
     )
     Scaffold (modifier = Modifier,
         topBar = {
             TopAppBar(
                 title = { Text(text = stringResource(id= R.string.tampil), color = Color.White)},
-                colors = TopAppBarDefaults.mediumTopAppBarColors(colorResource(id= R.color.teal_700))
+                colors = TopAppBarDefaults.mediumTopAppBarColors(colorResource(id= R.color.purple_500))
             )
         }){ isiRuang ->
         Column(modifier = Modifier.padding(isiRuang),
-            verticalArrangement = Arrangement.SpaceBetween){
-            Column(modifier = Modifier.padding(all = dimensionResource(id = R.dimen.padding_medium)),
-                verticalArrangement = Arrangement.spacedBy(dimensionResource(id = R.dimen.padding_small))) {
+            verticalArrangement = Arrangement.SpaceBetween
+        ){
+            Column(
+                modifier = Modifier.padding(all = dimensionResource(id = R.dimen.padding_medium)),
+                verticalArrangement = Arrangement.spacedBy(dimensionResource(id = R.dimen.padding_small))
+            ) {
                 items.forEach { item ->
                     Column {
                         Text(text = item.first.uppercase(), fontSize = 16.sp)
